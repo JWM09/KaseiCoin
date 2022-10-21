24c3c5221350f3ee8146f0144f1b1be597bc177d
JWM09 <jonm5214@hotmail.com>
2022-10-20 18:27:50 -1000

    Final commit

--- KaseiCoinCrowdsale.sol
+++ KaseiCoinCrowdsale.sol
@@ -1,51 +1,40 @@
 pragma solidity ^0.5.0;
-
 import "./KaseiCoin.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/Crowdsale.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/emission/MintedCrowdsale.sol";
-
-
-// Have the KaseiCoinCrowdsale contract inherit the following OpenZeppelin:
+// Bootstrap the KaseiCoinCrowdsale contract by inheriting the following OpenZeppelin:
 // * Crowdsale
 // * MintedCrowdsale
-contract KaseiCoinCrowdsale { // UPDATE THE CONTRACT SIGNATURE TO ADD INHERITANCE
-    
+contract KaseiCoinCrowdsale is Crowdsale, MintedCrowdsale {
     // Provide parameters for all of the features of your crowdsale, such as the `rate`, `wallet` for fundraising, and `token`.
     constructor(
-        // YOUR CODE HERE!
+        uint256 rate, // rate in TKNbits
+        address payable wallet, // sale beneficiary
+        KaseiCoin token // the KaseiCoin itself that the KaseiCoinCrowdsale will work with
     ) public Crowdsale(rate, wallet, token) {
         // constructor can stay empty
     }
 }
-
-/*
 contract KaseiCoinCrowdsaleDeployer {
-    // Create an `address public` variable called `kasei_token_address`.
-    // YOUR CODE HERE!
-    // Create an `address public` variable called `kasei_crowdsale_address`.
-    // YOUR CODE HERE!
 
-    // Add the constructor.
+    address public kasei_token_address;
+
+    address public kasei_crowdsale_address;
+   
     constructor(
-       // YOUR CODE HERE!
+        //string memory name,
+        //string memory symbol,
+        address payable wallet 
     ) public {
-        // Create a new instance of the KaseiCoin contract.
-        // YOUR CODE HERE!
-        
-        // Assign the token contract’s address to the `kasei_token_address` variable.
-        // YOUR CODE HERE!
+        KaseiCoin token = new KaseiCoin(0);
+        kasei_token_address = address(token);
+        KaseiCoinCrowdsale kasei_crowdsale =
+            new KaseiCoinCrowdsale(1, wallet, token);
 
-        // Create a new instance of the `KaseiCoinCrowdsale` contract
-        // YOUR CODE HERE!
-            
-        // Aassign the `KaseiCoinCrowdsale` contract’s address to the `kasei_crowdsale_address` variable.
-        // YOUR CODE HERE!
+        kasei_crowdsale_address = address(kasei_crowdsale);
 
-        // Set the `KaseiCoinCrowdsale` contract as a minter
-        // YOUR CODE HERE!
-        
-        // Have the `KaseiCoinCrowdsaleDeployer` renounce its minter role.
-        // YOUR CODE HERE!
+        token.addMinter(kasei_crowdsale_address);
+
+        token.renounceMinter();
     }
 }
-*/
--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # KaseiCoin
 UW Fintech Bootcamp Module 21 Challenge
+
+---
+
+## Technologies
+
+The following technology was used in this assignment:  
+  
+  * remix  
+  * ganache
+  * metamask  
+  
+---
+
+## Purpose of Project
+
+The main purpose of this project is to build a smart token and conduct a crowdsale.    
+  
+---
+
+## Evaluation Evidence
+
+The below images show the various required screen captures: 
+ 
+ ![Kasei Coin](Evaluation_Evidence/KaseiCoin_compiled.png)
+ 
+ ![Crowdsale](Evaluation_Evidence/KaseiCoinCrowdSale.png)
+  
+ ![Contract Call](Evaluation_Evidence/contract_call.png)  
+    
+ 
+Overall I can certainly see the value of of such technology and the ability to work with it enhancing my career opportunities as a fintech professional.  
+
+---
+
+## Contributors
+
+The majority of this project has been done independently, with assistance from my tutor Katie Kirby and instructor Gerrit Hall.
+
+---
+
+## License
+
+Program is free to use without license.  Only request is that you notify author of use and application.
+  
+To discuss usage or general inquires please contact the author at jonm5214@gmail.com
